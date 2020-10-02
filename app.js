cd74fb26786d3cf366bc97dab91ea5996725d31e
Mirzohid2226 <smm2226fbus@gmail.com>
2020-10-02 21:19:39 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+function toggleMenu(){
+	var menuTogle = document.querySelector('.toggle');
+	var menu = document.querySelector('.menu');
+	menuTogle.classList.toggle('active');
+	menu.classList.toggle('active');
+}
